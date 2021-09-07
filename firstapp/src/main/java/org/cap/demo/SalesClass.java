963dbf8161912f9f82c98425b49599706fe9dd17
capgteam <capgteam22020@gamil.com>
2021-09-07 16:15:28 +0530

    array added in sales class

--- firstapp/src/main/java/org/cap/demo/SalesClass.java
+++ firstapp/src/main/java/org/cap/demo/SalesClass.java
@@ -1,8 +1,14 @@
 package org.cap.demo;
 
 public class SalesClass {
+	int[] arr= {12,34,56,78,78};
+	
 	
 	public void show() {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
 		System.out.println("Sales Class Show method....");
 	}
 
