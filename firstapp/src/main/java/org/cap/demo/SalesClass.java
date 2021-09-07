e2df460b5c2b7a20e7c92fa7e759c7dea28235df
capgteam <capgteam22020@gamil.com>
2021-09-07 16:16:43 +0530

    sales class array included

--- firstapp/src/main/java/org/cap/demo/SalesClass.java
+++ firstapp/src/main/java/org/cap/demo/SalesClass.java
@@ -3,6 +3,16 @@ package org.cap.demo;
 public class SalesClass {
 	
 	public void show() {
+		
+		int[] arr= {12,3,4,5,67,67};
+		int sum=0;
+		
+		for(int n:arr) {
+			sum+=n;
+			System.out.println(n);
+		}
+		System.out.println("Summation: " + sum);
+		
 		System.out.println("Sales Class Show method....");
 	}
 
